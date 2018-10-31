7ea157936a0176c552d39d2ab6eba84cdf20fe30
Tola's Terminal <sohbaker@gmail.com>
2018-10-31 13:13:34 +0000

    if money is divisible into three equal coins, returns three coins

--- lib/coinchanger.rb
+++ lib/coinchanger.rb
@@ -2,15 +2,20 @@ def coinchanger(money)
 
   coins = [200, 100, 50, 20, 10, 5, 2, 1]
   not_coin = "not a coin"
+
   half = money / 2
-  remain = money % 2
+  remainhalf = money % 2
+  third =  money / 3
+  remainthird = money % 3
   # money_back = 0
 
   coins.each do |change|
     if money == change
       return money
-    elsif remain == 0 && half == change
+    elsif remainhalf == 0 && half == change
       not_coin = half, half
+    elsif remainthird == 0 && third == change
+      not_coin = third, third, third
     end
   end
   not_coin
--- spec/coinchanger_spec.rb
+++ spec/coinchanger_spec.rb
@@ -7,13 +7,13 @@ describe 'coinchanger' do
     end
   end
 
-  it 'returns "not a coin" when given 3' do
-    expect(coinchanger(3)).to eq "not a coin"
-  end
+  # it 'returns "not a coin" when given 3' do
+  #   expect(coinchanger(3)).to eq "not a coin"
+  # end
 
-  it 'returns "not a coin" when given a coin which is not in the array' do
-    expect(coinchanger(6)).to eq "not a coin"
-  end
+  # it 'returns "not a coin" when given a coin which is not in the array' do
+  #   expect(coinchanger(6)).to eq "not a coin"
+  # end
 
   it 'returns [2, 2] when given 4' do
     expect(coinchanger(4)).to eq [2, 2]
@@ -25,6 +25,12 @@ describe 'coinchanger' do
 
   it 'returns [200, 200] when given 400' do
     expect(coinchanger(400)).to eq [200, 200]
+  end
+
+  [600, 300, 150, 60, 30, 15, 6, 3].each do |money|
+    it 'returns 3 equaul coins when given money, if divisible by 3' do
+      expect(coinchanger(money)).to eq [money / 3] * 3
+    end
   end
 
 end
